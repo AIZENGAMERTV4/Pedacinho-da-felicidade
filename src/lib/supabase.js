7015daa255df6e4c65dd0595b496ac46ad9302ee
RayzerGamer <pf149429@gmail.com>
2026-07-30 19:15:33 -0300

    feat: upload de foto de produto no admin

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://lairdyjzddplgdbdolab.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhaXJkeWp6ZGRwbGdkYmRvbGFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDM4MjAsImV4cCI6MjEwMTAxOTgyMH0.xuNtw40m5PCTYOoNj6vL7D7IK8SsUkc8EiiontZeepM'
+const supabaseKey = 'sb_publishable_oFxznNYUpC8F91YvZp_EIA_EBxZRsQw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
